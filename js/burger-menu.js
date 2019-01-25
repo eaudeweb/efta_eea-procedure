1187a973ebb812bf736bfb469caf00b01b939733
Andrei Meliș <andrei.melis@eaudeweb.ro>
2019-01-25 18:24:03 +0200

    Testing WIP andrei

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,16 +1,15 @@
 let burgerMenu = document.getElementById("menu-toggler");
 let burgerMenuContainer = document.querySelector(".navbar .container");
-let navbarObtions = document.querySelector(".navbar .navbar-obtions");
+let navbarOptions = document.querySelector(".navbar .navbar-options");
 
 
 burgerMenu.addEventListener('click', () => {
-	console.log("HEi");
 	if(burgerMenuContainer.classList.contains('full-height')) {
 		burgerMenuContainer.classList.remove('full-height')
-		navbarObtions.classList.remove('flex-box')
+		navbarOptions.classList.remove('flex-box')
 
 	} else {
 		burgerMenuContainer.classList.add('full-height')
-		navbarObtions.classList.add('flex-box')
+		navbarOptions.classList.add('flex-box')
 	}
 })
